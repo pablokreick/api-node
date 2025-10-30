32af5b81683f9a8758ff3b03107204c7e8e60c38
pablokreick <pablokreick@gmail.com>
2025-10-30 00:14:16 -0300

    add endpoint singleton

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const characters = [
+  { id: 1, name: 'Rick Sanchez', species: 'Human', status: 'Alive' },
+  { id: 2, name: 'Morty Smith', species: 'Human', status: 'Alive' },
+  { id: 3, name: 'Birdperson', species: 'Bird-Person', status: 'Dead' },
+  { id: 4, name: 'Mr. Meeseeks', species: 'Meeseeks', status: 'Unknown' }
+];
+
+
 app.get('/character', (req, res) => {
-  res.json({
-    name: 'Axl',
-    class: 'Rogue',
-    level: 7
-  });
+    const {name} = req.query
+    let result = characters;
+    if (name) {
+        result = characters.filter(character => character.name.toLowerCase().includes(name.toLowerCase()))
+    }
+  res.json(result);
 });
+
+app.get('/character/:id', (req, res)=> {
+    const {id} = req.query
+    const character = characters.find((c)=> c.id === Number(id))
+
+    if(!character) return res.status(404).json({error: 'Character not found'})
+
+    res.json(character)
+})
 
 app.get('/', (req, res) => {
   res.send('API de personajes lista para usar 🚀');
 });
 
-app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
-});
+app.listen(PORT);
